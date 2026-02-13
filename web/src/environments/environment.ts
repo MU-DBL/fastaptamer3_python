4f47b7061bb991cbbcbc52416c5c4e4fae514329
Yongfang Qin <yongfang012@gmail.com>
2026-02-13 09:26:45 -0600

    fix bugs

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: '/api/v1',
+  apiUrl: 'http://localhost:5001/api/v1',
   appName: 'FASTAptameR 3.0',
   enableDebug: true,
   version: '1.0.0-dev'
